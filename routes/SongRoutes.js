5273198da00285e733fd0658c9d72fe321ed4a5c
narayanLohani <narayanlohani07@gmail.com>
2025-08-25 01:52:25 +0545

    added route to get shuffled and default songs

--- routes/SongRoutes.js
+++ routes/SongRoutes.js
@@ -1,12 +1,39 @@
 import express from "express";
 import Song from "../models/Songs.js";
+import { shuffleWithSeed } from "../utils/seed.js";
 
 const router = express.Router();
+
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
 
 router.get("/", async (req, res) => {
     try {
         const songs = await Song.find();
         res.json(songs);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get("/random", async (req, res) => {
+    try {
+        const songs = await Song.find();
+        res.json(shuffleArray(songs));
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get("/default", async (req, res) => {
+    try {
+        const songs = await Song.find();
+        res.json(shuffleWithSeed(songs));
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
